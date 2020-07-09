5c716a515d39cb6758c44ab265123ed46be9c8e5
Jonas Wagner <ltlygwayh@gmail.com>
2020-07-10 00:24:59 +0200

    Our father prayer for adults.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
+function setElementText(element, text) {
+  element.velocity({opacity : 0}, {
+    duration : 400,
+    complete : () => {
+      element.textContent = text;
+      element.velocity({opacity : 1}, {duration : 400});
+    }
+  });
+}
+
 function replacePart(part) {
   const paragraph = document.getElementById(`gebet-${part}`);
   const options = kGebete.get(part);
   const choice = Math.floor(Math.random() * options.length);
-  paragraph.textContent = options[choice];
+  setElementText(paragraph, options[choice]);
 }
 
 function cyclePart(part) {
   const paragraph = document.getElementById(`gebet-${part}`);
   const options = kGebete.get(part);
   const current = options.findIndex((t) => t === paragraph.textContent);
-  paragraph.textContent = options[(current + 1) % options.length];
+  setElementText(paragraph, options[(current + 1) % options.length]);
 }
 
 const gebetRandom = document.getElementById('gebet-random');
@@ -20,7 +30,6 @@ gebetRandom.addEventListener('click', (event) => {
 });
 
 for (const part of kGebete.keys()) {
-  document.getElementById(`gebet-${part}`).addEventListener('click', (event) => {
-    cyclePart(part);
-  });
+  document.getElementById(`gebet-${part}`)
+      .addEventListener('click', (event) => { cyclePart(part); });
 }
