f37bafb5a6f3744ee59e83a2e2cb2ec3d5ca2e32
Jonas Wagner <ltlygwayh@gmail.com>
2021-02-28 18:31:53 +0100

    Add a tutorial :)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
+function tutorial(part) {
+  const pointer = document.getElementById('tutorial');
+  const paragraph = document.getElementById('gebet-' + part);
+
+  function part0Prepare() {
+    const pos = paragraph.getBoundingClientRect();
+
+    pointer.style.opacity = 0;
+    pointer.style.display = 'block';
+    pointer.style.left = (pos.left + 20) + 'px';
+    pointer.style.top = (pos.top + pos.height / 2) + 'px';
+  }
+
+  function part1MouseDown() {
+    pointer.classList.add('far');
+    pointer.classList.remove('fas');
+  }
+
+  function part2MouseUp() {
+    pointer.classList.add('fas');
+    pointer.classList.remove('far');
+  }
+
+  pointer
+    .velocity({}, {duration: 2000, complete: part0Prepare})
+    .velocity({opacity: 1}, {duration: 400})
+    .velocity({}, {duration: 1000, complete: part1MouseDown})
+    .velocity({}, {duration: 200, complete: part2MouseUp})
+    .velocity({}, {duration: 200})
+    .velocity({opacity: 0}, {duration: 200, complete: () => cyclePart(part)})
+}
+
 function setElementText(element, text) {
   element.velocity({opacity : 0}, {
     duration : 400,
